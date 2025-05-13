19e2a710192defeaea540ebb55c8ae4199875591
marcopriests <prtmrc98@gmail.com>
2025-05-13 16:39:50 +0200

    [UPD] sistemato un po' di cose

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Card from "./components/Card"
 function App() {
   const [actress, setActress] = useState([])
   const [actors, setActors] = useState([])
+  const [allActors, setAllActors] = useState([])
+  const [value, setValue] = useState('')
+  const [filteredActors, setFilteredActors] = useState(allActors)
 
   const fetchActress = () => {
     axios.get('https://lanciweb.github.io/demo/api/actresses/').then(res => {
@@ -18,33 +21,32 @@ function App() {
     })
   }
 
+  const render = () => {
+    setAllActors([...actress])
+  }
+
 
   useEffect(() => {
     fetchActress()
     fetchActors()
-  }, [])
+    render()
+    
+    const filtered = allActors.filter((act) => {
+      return act.name.toLowerCase().includes(value.toLowerCase())
+    })
 
+    setFilteredActors(filtered)
+  }, [value])
 
   return (
     <>
       <div className="container p-5">
         <h1 className="fw-bold mb-5">Hall of fame</h1>
 
-        <input type="text" className="form-control form-control-lg border-0 shadow-sm mb-5" placeholder="Search..." />
-
-        <h3 className="mb-3">Actresses</h3>
+        <input type="text" className="form-control form-control-lg border-0 shadow-sm mb-5" placeholder="Search..." value={value} onChange={e => setValue(e.target.value)} />
         <div className="row  row-cols-3 mb-5">
 
-          {actress.map((act) => {
-            return <Card act={act} key={act.id} />
-          })}
-
-        </div>
-
-        <h3 className="mb-3">Actors</h3>
-        <div className="row  row-cols-3">
-
-          {actors.map((act) => {
+          {allActors.map((act) => {
             return <Card act={act} key={act.id} />
           })}
 
